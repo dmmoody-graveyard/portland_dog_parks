da82277e6a290dd77b188bcd56b9a2b278b79821
Duane M. Moody <dmmoody@gmail.com>
2015-03-11 15:52:14 -0700

    refactor controller

--- public/app/controllers/ParkController.js
+++ public/app/controllers/ParkController.js
@@ -1,4 +1,4 @@
-dogParks.controller('ParkCtrl', ['$scope', '$http','$state', function($scope, $http, $state) {
+dogParks.controller('ParkCtrl', ['$scope', '$http', '$state', function($scope, $http, $state) {
   var url = '/parks/' + $state.params.parkID
   var park;
   $http.get(url).success (function(data){
