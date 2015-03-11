1924577b8a5c053e8b54ac9e6ee4c446c13d5579
Duane M. Moody <dmmoody@gmail.com>
2015-03-11 10:23:13 -0700

    fix angular routing conventions

--- public/app/app.js
+++ public/app/app.js
@@ -3,11 +3,17 @@ var dogParks = angular.module('dogParks', ['ui.router']);
 dogParks.config(function($stateProvider) {
   $stateProvider.state('home', {
     url: "",
-    templateUrl: "partials/home.html"
+    templateUrl: "partials/home.html",
   });
 
+  $stateProvider.state('parks', {
+    url: "/parks",
+    templateUrl: 'partials/parks.html',
+    controller: "dogParksCtrl"
+  })
+
   $stateProvider.state('park', {
-    url: "/:parkID",
+    url: "parks/:parkID",
     templateUrl: "partials/park.html",
     controller: "ParksCtrl"
   });
